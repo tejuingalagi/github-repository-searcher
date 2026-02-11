ad0ff72fad43174b2fe16394e6762cb8e2db0294
tejuingalagi <tejuingalagi07@gmail.com>
2026-02-11 11:34:24 +0530

    feat: add github api exception and global exception handler

--- src/main/java/com/freightfox/githubsearcher/exception/GithubApiException.java
+++ src/main/java/com/freightfox/githubsearcher/exception/GithubApiException.java
@@ -1,5 +1,12 @@
 package com.freightfox.githubsearcher.exception;
 
-public class GithubApiException {
+public class GithubApiException extends RuntimeException {
 
+    public GithubApiException(String message) {
+        super(message);
+    }
+
+    public GithubApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
